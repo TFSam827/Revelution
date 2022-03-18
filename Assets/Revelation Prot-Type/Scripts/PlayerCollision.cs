e0140b35612fb40e2485f613cbd37ac5eadcbece
TFSam827 <samuel@sbenting.net>
2022-03-18 08:15:29 -0700

    Level-2

--- Assets/Revelation Prot-Type/Scripts/PlayerCollision.cs
+++ Assets/Revelation Prot-Type/Scripts/PlayerCollision.cs
@@ -4,6 +4,9 @@ public class PlayerCollision : MonoBehaviour
 {
     void OnCollisionEnter2D (Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "lava")
+        {
+            Destroy(gameObject);
+        }
     }
 }
