8dd87e9970bb141fde5aad60972740f39e817303
TFSam827 <samuel@sbenting.net>
2022-03-21 10:14:42 -0700

    levels

--- Assets/Revelation Prot-Type/Scripts/PlayerBehavior.cs
+++ Assets/Revelation Prot-Type/Scripts/PlayerBehavior.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerBehavior : MonoBehaviour
 {
@@ -9,14 +8,6 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Finish")
-        {
-            SceneManager.LoadScene("Type-2");
-        }
     }
 }
