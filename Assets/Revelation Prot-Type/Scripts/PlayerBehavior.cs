02018f11c127a778d6abc5b038e87a4e8dbd15f0
TFSam827 <samuel@sbenting.net>
2022-03-22 10:17:26 -0700

    proto-type completed

--- Assets/Revelation Prot-Type/Scripts/PlayerBehavior.cs
+++ Assets/Revelation Prot-Type/Scripts/PlayerBehavior.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-
+    public GameObject spawner;
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.y < -6)
+        {
+            transform.position = spawner.transform.position;
+        }
     }
 }
