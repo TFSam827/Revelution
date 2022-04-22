ffbbbe1d69736be675f1a21ab96caecdedfde079
TFSam827 <samuel@sbenting.net>
2022-04-22 14:02:17 -0700

    camera

--- Assets/Revelation Prot-Type/Scripts/PlayerMovement.cs
+++ Assets/Revelation Prot-Type/Scripts/PlayerMovement.cs
@@ -1,13 +1,17 @@
 //Credit Brackeys
 //Link: https://www.youtube.com/watch?v=dwcT-Dch0bA
+//Edited by Samuel Benting
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+
+    PlayerControls controls;
 
     public CharacterController2D controller;
 
@@ -19,45 +23,62 @@ public class PlayerMovement : MonoBehaviour
     bool jump = false;
     bool crouch = false;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        controls = new PlayerControls();
+
+        controls.Gameplay.Left.performed += ctx => Left();
+        controls.Gameplay.Right.performed += ctx => Right();
+        controls.Gameplay.Jump.performed += ctx => Jump();
+        controls.Gameplay.Down.started += ctx => CrouchOn();
+        controls.Gameplay.Down.canceled += ctx => CrouchOff();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalmove = Input.GetAxisRaw("Horizontal") * speed;
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
-
-        if (Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
-        } else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
-        } else if (Input.GetButtonDown("Dash"))
-        {
-            if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                rb.velocity = Vector2.right * 200;
-                while (dashTime > 0)
-                {
-                    rb.isKinematic = true;
-                }
-                rb.isKinematic = false;
-                rb.velocity = Vector2.right * 0;
-            }
-            
-        }
 
     }
 
-    void FixedUpdate() {
+    void Left()
+    {
+        horizontalmove = -1 * speed;
+    }
+
+    void Right()
+    {
+        horizontalmove = 1 * speed;
+    }
+
+    void Jump()
+    {
+        jump = true;
+    }
+
+    void CrouchOn()
+    {
+        crouch = true;
+    }
+
+    void CrouchOff()
+    {
+        crouch = false;
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    void FixedUpdate()
+    {
 
         controller.Move(horizontalmove * Time.fixedDeltaTime, crouch, jump);
 
--- Assets/Revelation Prot-Type/Scripts/start.cs
+++ Assets/Revelation Prot-Type/Scripts/start.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class start : MonoBehaviour
 {
-    void Update()
+
+    PlayerControls controls;
+
+    void Awake()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            SceneManager.LoadScene("level-1");
-        }
+        controls = new PlayerControls();
+
+        controls.Gameplay.Start.performed += ctx => Start();
+    }
+
+    void Start()
+    {
+        SceneManager.LoadScene("level-1");
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
     }
 }   
 
