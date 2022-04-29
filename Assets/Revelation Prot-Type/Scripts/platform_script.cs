ee410f16045d3f57b85303fb024ea25a6b78eba8
TFSam827 <samuel@sbenting.net>
2022-04-29 10:06:50 -0700

    elivator

--- Assets/Revelation Prot-Type/Scripts/platform_script.cs
+++ Assets/Revelation Prot-Type/Scripts/platform_script.cs
@@ -4,23 +4,58 @@ using UnityEngine;
 
 public class platform_script : MonoBehaviour
 {
+    PlayerControls controls;
 
-    Rigidbody2D rb;
+    bool move = false;
+    bool top = false;
+    bool bottom = false;
 
-    void Start()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.isKinematic = true;
+        controls = new PlayerControls();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (Input.GetButtonDown("Crouch"))
-            {
-                rb.isKinematic = false;
-            }
+            move = true;
+        } else if (collision.gameObject.tag == "top")
+        {
+            top = true;
+        } else if (collision.gameObject.tag == "bottom")
+        {
+            bottom = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            move = false;
+        } else if (collision.gameObject.tag == "top")
+        {
+            top = false;
+        } else if (collision.gameObject.tag == "bottom")
+        {
+            bottom = false;
+        }
+    }
+
+    void Down()
+    {
+        while (move && !top)
+        {
+            
+        }
+    }
+
+    void Up()
+    {
+        while (move && !bottom)
+        {
+
         }
     }
 }
