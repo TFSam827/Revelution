e15fe44de046488049508e09745080a4bbf4aebf
TFSam827 <samuel@sbenting.net>
2022-06-06 17:31:09 -0700

    New

--- Assets/Revelation Prot-Type/Scripts/Start.cs
+++ Assets/Revelation Prot-Type/Scripts/Start.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class click : MonoBehaviour
+public class Start : MonoBehaviour
 {
     PlayerControls controls;
 
@@ -12,11 +12,11 @@ public class click : MonoBehaviour
     {
         controls = new PlayerControls();
 
-        controls.Gameplay.Click.performed += ctx => Click();
+        controls.Gameplay.Enter.performed += ctx => Enter();
     }
 
-    void Click()
+    void Enter()
     {
-
+        SceneManager.LoadScene("Level-Select");
     }
 }
